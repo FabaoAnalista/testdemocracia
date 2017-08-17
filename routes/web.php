321878a92fd845aad7b132a83f84274fec4316c6
Fabio Humberto <fabaoanalista@gmail.com>
2017-08-17 02:16:45 -0300

    Alterações em arquivos de configuração e rotas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/home', function () {
     return view('welcome');
 });
@@ -25,12 +14,8 @@ Route::get('dashboard', function () {
 Route::get('template', function () {
     return view('template');
 });
+Route::resource('/projetos', 'ProjetosController');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/', function () {
-    return view('principal');
-});
-Route::resource('/projetos', 'ProjetosController');
