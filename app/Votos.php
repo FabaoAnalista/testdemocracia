754e9cd4269bfd27334473056cc50d2e88a362de
otaviodecastro <otaviognr@hotmail.com>
2017-08-20 13:37:46 -0300

    model Votos atualizado

--- app/Votos.php
+++ app/Votos.php
@@ -8,5 +8,12 @@ class Votos extends Model
 {
   protected $primaryKey = 'idVoto';
   protected $table = 'votos';
-  protected $fillable = ['users_id','projeto_idProjeto','quantidade'];
+  protected $fillable = ['users_id','projeto_idProjeto'];
+  
+    public function projetos(){
+    	return $this->belongsTo('App\Projetos');
+    }
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }
 }
